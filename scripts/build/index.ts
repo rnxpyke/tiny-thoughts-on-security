0ca280e947896c7b4a5d0c4fd58b23a6fc756d6d
rnxpyke <rnxpyke@gmail.com>
2024-08-06 18:11:12 +0200

    add build script

--- scripts/build/index.ts
+++ scripts/build/index.ts
@@ -5,10 +5,32 @@ const mds = new Glob('**/*.md');
 
 const outdir = "./out";
 
-for await (const file of mds.scan(".")) {
-  const target = file.replace('.md$', '.html');
-  const targetDir = path.dirname(target);
-  console.log(file, target);
+function markdownToHtml(file: string) {
+  return file.replace(/\.md$/, '.html');
+}
+
+async function buildMarkdown(file: string, out: string) {
+  const targetDir = path.dirname(out);
   await $`mkdir -p ${targetDir}`;
-  await $`pandoc -t html -f commonmark_x ${file}`;
+  const buffer = await ($`pandoc -s -t html -f commonmark_x ${file}`.bytes());
+  const rewriter = new HTMLRewriter();
+  rewriter.on('a', {
+    element(e) {
+      const href = e.getAttribute('href');
+      if (!href) return;
+      if (!href.startsWith('./')) return;
+      if (!href.endsWith('.md')) return;
+      const newRef = href.replace(/\.md$/, '.html');
+      e.setAttribute('href', newRef);
+    }
+  })
+  const html = rewriter.transform(buffer);
+  Bun.write(out, html);
+}
+
+await buildMarkdown('README.md', path.join(outdir, 'index.html'));
+for await (const file of mds.scan("./thoughts")) {
+  const inp = path.join('./thoughts', file);
+  const out = path.join(outdir, markdownToHtml(inp));
+  await buildMarkdown(inp, out);
 }
